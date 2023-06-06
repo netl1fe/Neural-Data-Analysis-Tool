3ea9e791329c5a2a99e769cc011253a26ed259fd
netl1fe <127271713+netl1fe@users.noreply.github.com>
2023-06-06 15:04:11 -0400

    Update v0.py

--- v0.py
+++ v0.py
@@ -11,31 +11,42 @@ bands = {'Delta': (0.5, 4),
          'Beta': (15, 30),
          'Gamma': (30, 45)}
 
-# Load the edf file
-edf_file = os.path.join('.', '01_sleep_psg.edf')
-raw = mne.io.read_raw_edf(edf_file, preload=True)  # preload data
+# Get the current directory
+current_dir = os.getcwd()
 
-# Apply the bandpass filter to the raw data
-raw_filt = {band: raw.copy().filter(l_freq, h_freq) for band, (l_freq, h_freq) in bands.items()}
+# Get all files in the current directory
+files = os.listdir(current_dir)
 
-# Create plotly graph object
-fig = go.Figure()
+# Filter EDF files
+edf_files = [file for file in files if file.endswith(".edf")]
 
-# Iterate over the bands and add to the plot
-for band, raw_band in raw_filt.items():
-    # Get the psd for this band
-    psd, freqs = psd_array_welch(raw_band.get_data(), raw.info['sfreq'], fmin=0.5, fmax=45.)
-    
-    # Average across channels
-    psd_mean = psd.mean(axis=0)
-    
-    # Add to the plotly figure
-    fig.add_trace(go.Scatter(x=freqs, y=10 * np.log10(psd_mean), mode='lines', name=band))
+# Process each EDF file
+for edf_file in edf_files:
+    # Load the edf file
+    file_path = os.path.join(current_dir, edf_file)
+    raw = mne.io.read_raw_edf(file_path, preload=True)  # preload data
 
-# Set the title and axis labels
-fig.update_layout(title='Power Spectral Density (PSD) per frequency band',
-                  xaxis_title='Frequency (Hz)',
-                  yaxis_title='Power Spectral Density (dB)')
+    # Apply the bandpass filter to the raw data
+    raw_filt = {band: raw.copy().filter(l_freq, h_freq) for band, (l_freq, h_freq) in bands.items()}
 
-# Display the figure
-fig.show()
+    # Create plotly graph object
+    fig = go.Figure()
+
+    # Iterate over the bands and add to the plot
+    for band, raw_band in raw_filt.items():
+        # Get the psd for this band
+        psd, freqs = psd_array_welch(raw_band.get_data(), raw.info['sfreq'], fmin=0.5, fmax=45.)
+
+        # Average across channels
+        psd_mean = psd.mean(axis=0)
+
+        # Add to the plotly figure
+        fig.add_trace(go.Scatter(x=freqs, y=10 * np.log10(psd_mean), mode='lines', name=band))
+
+    # Set the title and axis labels
+    fig.update_layout(title=f"Power Spectral Density (PSD) per frequency band - {edf_file}",
+                      xaxis_title='Frequency (Hz)',
+                      yaxis_title='Power Spectral Density (dB)')
+
+    # Display the figure
+    fig.show()
