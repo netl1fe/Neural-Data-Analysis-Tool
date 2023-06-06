987390b975c1b60c40b94bf3c513de4a021f4c08
netl1fe <127271713+netl1fe@users.noreply.github.com>
2023-06-06 14:50:01 -0400

    Update v0.py

--- v0.py
+++ v0.py
@@ -1,35 +1,40 @@
+import os
+import numpy as np
 import mne
-from mne.preprocessing import ICA
-import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
-class NeuralDataAnalysisTool:
-    def __init__(self, file_path):
-        self.raw = mne.io.read_raw_edf(file_path, preload=True)
-        
-    def visualize_raw_data(self, start=0, duration=10):
-        """Visualize the first `duration` seconds of the recording."""
-        self.raw.plot(duration=duration, start=start)
-        
-    def plot_psd(self, fmin=0, fmax=50):
-        """Compute and plot the power spectral density (PSD)."""
-        self.raw.plot_psd(fmin=fmin, fmax=fmax)
-        
-    def perform_ica(self, n_components=0.95):
-        """
-        Perform Independent Component Analysis (ICA) to identify components
-        corresponding to eye blinks or other artifacts.
-        """
-        ica = ICA(n_components=n_components, method='fastica')
-        ica.fit(self.raw)
-        
-        # Plot the independent components
-        ica.plot_components(outlines='head', ch_type='eeg')
-        
-        # Plot properties of the first five independent components
-        ica.plot_properties(self.raw, picks=range(5))
+# Define the band-pass filter properties
+bands = {'Delta': (0.5, 4),
+         'Theta': (4, 8),
+         'Alpha': (8, 15),
+         'Beta': (15, 30),
+         'Gamma': (30, 45)}
 
-# Usage
-tool = NeuralDataAnalysisTool('path_to_your_data.edf')
-tool.visualize_raw_data()
-tool.plot_psd()
-tool.perform_ica()
+# Load the edf file
+edf_file = os.path.join('.', '01_sleep_psg.edf')
+raw = mne.io.read_raw_edf(edf_file, preload=True)  # preload data
+
+# Apply the bandpass filter to the raw data
+raw_filt = {band: raw.copy().filter(l_freq, h_freq) for band, (l_freq, h_freq) in bands.items()}
+
+# Create plotly graph object
+fig = go.Figure()
+
+# Iterate over the bands and add to the plot
+for band, raw_band in raw_filt.items():
+    # Get the psd for this band
+    psd, freqs = mne.time_frequency.psd_multitaper.psd_multitaper(raw_band, fmin=0.5, fmax=45.)
+    
+    # Average across channels
+    psd_mean = psd.mean(axis=0)
+    
+    # Add to the plotly figure
+    fig.add_trace(go.Scatter(x=freqs, y=psd_mean, mode='lines', name=band))
+
+# Set the title and axis labels
+fig.update_layout(title='Power Spectral Density (PSD) per frequency band',
+                  xaxis_title='Frequency (Hz)',
+                  yaxis_title='Power Spectral Density (dB)')
+
+# Display the figure
+fig.show()
