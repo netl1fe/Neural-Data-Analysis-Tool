5357cdaedbef733538778c578ed452986f55aec8
net1ife <127271713+net1ife@users.noreply.github.com>
2023-06-23 23:46:16 -0400

    Update v0.py

--- v0.py
+++ v0.py
@@ -1,8 +1,10 @@
 import os
 import numpy as np
+import pandas as pd
 import mne
 from mne.time_frequency import psd_array_welch
 import plotly.graph_objects as go
+import plotly.io as pio
 
 # Define the band-pass filter properties
 bands = {'Delta': (0.5, 4),
@@ -20,25 +22,30 @@ files = os.listdir(current_dir)
 # Filter EDF files
 edf_files = [file for file in files if file.endswith(".edf")]
 
+# Create a DataFrame to store average power for each band and each file
+avg_power_df = pd.DataFrame(columns=['file'] + list(bands.keys()))
+
 # Process each EDF file
 for edf_file in edf_files:
     # Load the edf file
     file_path = os.path.join(current_dir, edf_file)
     raw = mne.io.read_raw_edf(file_path, preload=True)  # preload data
 
-    # Apply the bandpass filter to the raw data
-    raw_filt = {band: raw.copy().filter(l_freq, h_freq) for band, (l_freq, h_freq) in bands.items()}
-
     # Create plotly graph object
     fig = go.Figure()
 
-    # Iterate over the bands and add to the plot
-    for band, raw_band in raw_filt.items():
-        # Get the psd for this band
-        psd, freqs = psd_array_welch(raw_band.get_data(), raw.info['sfreq'], fmin=0.5, fmax=45.)
+    avg_power = {'file': edf_file}
 
+    # Iterate over the bands
+    for band, (l_freq, h_freq) in bands.items():
+        # Apply the bandpass filter to the raw data
+        raw_band = raw.copy().filter(l_freq, h_freq)
+        # Get the psd for this band
+        psd, freqs = psd_array_welch(raw_band.get_data(), raw.info['sfreq'], fmin=l_freq, fmax=h_freq)
         # Average across channels
         psd_mean = psd.mean(axis=0)
+        # Calculate average power for the band
+        avg_power[band] = 10 * np.log10(psd_mean.mean())
 
         # Add to the plotly figure
         fig.add_trace(go.Scatter(x=freqs, y=10 * np.log10(psd_mean), mode='lines', name=band))
@@ -48,5 +55,12 @@ for edf_file in edf_files:
                       xaxis_title='Frequency (Hz)',
                       yaxis_title='Power Spectral Density (dB)')
 
-    # Display the figure
-    fig.show()
+    # Save the figure to a PNG file
+    pio.write_image(fig, f"{edf_file}_psd.png")
+
+    # Add the average power to the DataFrame
+    avg_power_df = avg_power_df.append(avg_power, ignore_index=True)
+
+# Save the DataFrame to a CSV file
+avg_power_df.to_csv('avg_power.csv', index=False)
+
